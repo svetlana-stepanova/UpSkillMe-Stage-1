6b1ad2a8f6cf0135ff8d207f09e6accac31bf3f8
Svetlana Stepanova <svetlana.grishanova@gmail.com>
2022-08-03 20:46:50 +0500

    Add Conditional statements and Data types Practical task.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UpSkillMe-Stage-1
 Practical tasks
+
+task1 - Conditional statements and Data types Practical task.
---
+++ task1/file.js
@@ -0,0 +1,33 @@
+// 1.Perform addition of various types (string + boolean, string + number, number + boolean)
+console.log('Hello' + true);
+console.log('Hello' + 10);
+console.log(10 + true);
+
+// 2.Perform multiplication of various types (string * boolean, string * number, number * boolean)
+console.log('Hello' * true);
+console.log('Hello' * 10);
+console.log(10 * true);
+
+// 3.Divide different types (string / boolean, string / number, number / Boolean)
+console.log('Hello' / true);
+console.log('Hello' / 10);
+console.log(10 / true);
+
+//4.Perform explicit conversion (number, string, boolean)
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number('Hello'));
+console.log(Number('123'));
+console.log(Number(' '));
+console.log(Number('null'));
+console.log(Number(null));
+console.log(Number(undefined));
+console.log(String(true));
+console.log(String(123));
+console.log(Boolean('Hello'));
+console.log(Boolean('1'));
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(' '));
+console.log(Boolean(''));
